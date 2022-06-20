e4ff603b8c587ee0808ef1511a27411fe79bfb9e
msolimann2cs <msolimann2cs@gmail.com>
2022-06-20 20:17:02 +0200

    Updated design to the one provided.

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,100 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
-        body: MainBody(),
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
 
-class MainBody extends StatelessWidget {
-  const MainBody({Key? key}) : super(key: key);
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
 
+class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        Container(
-          height: 484,
-          color: Colors.black,
-          child: const Center(
-            child: Text(
-              'This is where the question text will go',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 25,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where the question text will go.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
         ),
-        Container(
-          width: double.infinity,
-          height: 75,
-          margin: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-          color: Colors.green,
-          child: const Center(
-            child: Text(
-              'True',
-              style: TextStyle(
-                fontSize: 25,
-                color: Colors.white,
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
               ),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+              },
             ),
           ),
         ),
-        Container(
-          width: double.infinity,
-          height: 75,
-          margin: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-          color: Colors.red,
-          child: const Center(
-            child: Text(
-              'False',
-              style: TextStyle(
-                fontSize: 25,
-                color: Colors.white,
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
               ),
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+              },
             ),
           ),
         ),
+        //TODO: Add a Row here as your score keeper
       ],
     );
   }
 }
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
